023bdd5425b151c5356a513d15a5c2c9a227c658
laurabotel <laura.e.botel@gmail.com>
2021-11-02 16:02:57 -0400

    added function to find a record by its value

--- server/controllers/databaseController.js
+++ server/controllers/databaseController.js
@@ -3,9 +3,11 @@ const url = require('url');
 
 const databaseController = {};
 
-// takes an object of key value pairs to insert as row into db table
-// where key is name of column and value is associated value
-// returns SQL string
+
+///////// SQL STRING GENERATORS /////////
+// return SQL strings
+
+// takes an object of key value pairs to insert as row into db table where key is name of column and value is associated value
 const insertRowSQL = (table, colsVals) => {
   const cols = Object.keys(colsVals);
   const vals = [];
@@ -17,10 +19,27 @@ const insertRowSQL = (table, colsVals) => {
   return `INSERT INTO ${table} (${cols}) VALUES (${vals})`
 }
 
+// takes a table and an id to search by
+const findByValue = (table, col, val) => {
+  return `SELECT * FROM ${table} WHERE ${col} = ${val}`;
+}
+
+console.log(findByValue('users', '_id', 1000));
+
+///////// USERS /////////
+// fields for users table:
+  // _id
+  // user_token
+  // first_name
+  // last_name
+  // email_address
+  // profile_picture
+  // connections
+
 
 databaseController.findUser = async (req, res, next) => {
   const user_id = req.params.user_id; // receive from front end
-  const queryString = `SELECT * FROM users WHERE _id = ${user_id}`
+  const queryString = findByValue('users', '_id', user_id);
 
   try {
     await db.query(queryString)
@@ -62,25 +81,60 @@ databaseController.getConnections = async (req, res, next) => {
   }
 };
 
-// GROUPS
-// add group to db
-// INSERT INTO groups (_group_id, group_name, contact_freq) VALUES (4000, 'High Priority', 'Monthly')
+///////// GROUPS /////////
 // fields for groups table:
   // _group_id
   // group_name
   // contact_freq
 
+// find group in db --> SELECT * FROM "public"."groups" WHERE _group_id = PASS IN GROUP ID
+databaseController.findGroup = async (req, res, next) => {
+  const group_id = req.params.group_id; // receive from front end
+  const queryString = `SELECT * FROM users WHERE _id = ${group_id}`
+
+  try {
+    await db.query(queryString)
+      .then(groupData => {
+        console.log(groupData);
+        res.locals.groupData = groupData;
+      });
+    // what happens when the group isn't found in the db??
+    next();  
+  }
+  catch {
+    next({
+      log: 'Error in databaseController.findGroup',
+      status: 500,
+      message: {err: 'Error in databaseController.findGroup'},
+    });
+  }
+};
+
+// get all groups in db --> SELECT * FROM "public"."groups" LIMIT 100
+
+// add group to db --> INSERT INTO groups (_group_id, group_name, contact_freq) VALUES (4000, 'High Priority', 'Monthly')
+databaseController.addGroup = async (req, res, next) => {
+  // expects object with keys _group_id, group_name, contact_freq and their associated values
+  const group_info = req.params.group_info;
+  const queryString = insertRowSQL('groups', group_info);
+
+  try {
+    await db.query(queryString);
+    next();
+  }
+  catch {
+    next({
+      log: 'Error in databaseController.addGroup',
+      status: 500,
+      message: {err: 'Error in databaseController.addGroup'},
+    });
+  }
+};
+
 // USERS
 // add user to db
 // INSERT INTO users (_id, first_name, last_name, email_address) VALUES (1000, 'lu', 'pin', 'lupe@in.com')
-// fields for users table:
-  // _id
-  // user_token
-  // first_name
-  // last_name
-  // email_address
-  // profile_picture
-  // connections
+
 
 // CONNECTIONS
 // edit connection in db (returns row that was updated)
