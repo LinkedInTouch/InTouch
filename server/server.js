2c13e3c7e070f8c763b2536d92ea2da76b2b9df2
joelparkk <joelpark97@gmail.com>
2021-11-02 12:40:01 -0400

    fixed server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,33 @@
-const app = require("./app");
-const port = 3000
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+app.get('/', (req,res) => {
+  res.sendStatus(200);
+})
+// app.post('https://www.linkedin.com/oauth/v2/accessToken', (req,res) => {
+  
+// })
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
 
-
-app.listen(port, () => console.log(`Listening on Port ${port}`));
-module.exports = port;
+module.exports = app;
