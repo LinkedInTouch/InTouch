f79d38e74b167a3032cafcd58f894bab48a17e51
joelparkk <joelpark97@gmail.com>
2021-11-02 19:25:32 -0400

    working on linkedin Oauth and login page

--- server/server.js
+++ server/server.js
@@ -6,12 +6,26 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../client')));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //cors
+  next();
+});
 
-app.get('/', (req,res) => {
-  res.sendStatus(200);
+app.get('/api', (req,res) => {
+  console.log('req',req.query.code);
+  res.locals.accessToken = req.query.code;
 })
-// app.post('https://www.linkedin.com/oauth/v2/accessToken', (req,res) => {
+
+app.get('/api', (req,res) => {
+  res.sendFile(path.resolve(__dirname,'../src/index.html'));
+})
   
+
+// app.post('https://www.linkedin.com/oauth/v2/accessToken', (req,res) => {
+//   req.body = {
+//     grant_type
+//   }
 // })
 
 app.use((err, req, res, next) => {
--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import React,{useEffect} from 'react';
 
 export default function LogIn() {
+    const accessToken = 'AQS0vtZDhj9hwyoOUM0Y5UJYgJuBA3jH_BK8_7SM4n19Qbvge2GStqMbpx96cZwb7YN9sXZLfAScIPoepJzofIh6dAT_zF747ib-HVQCMrM5YnwiU5LtV0wperWlotXl_G5Mp4eDFHwSXGKgrnUxxyYZqZtfhagmEmQukRHChE4AQD7inCtPwyRJZCL_UWRzqqoIwG-Io0SNiMagkEo'
+    // const getAccess = async () => {
+    //     try{
+    //         await axios({
+    //             method: "GET",
+    //             url: "https://www.linkedin.com/oauth/v2/accessToken",
+    //             headers: {
+    //                 "Authorization": "Bearer " + accessToken
+    //             }
+    //         })
+    //         .then(data => console.log(data))
+    //     }
+    //     catch(err){
+    //         console.log(err);
+    //     }
+ 
+    // }
+    // useEffect(() => {
+    //     getAccess();
+    // },[])
     return (
         <div>
             This is the login page. 
+            <a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=r_liteprofile&client_id=77l2yl9ia652i4&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi">To signup through LinkedIn Click Here</a>
         </div>
     )
 }
