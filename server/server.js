62c50e67c9bf480a1d603c934d6243773747c9e5
laurabotel <laura.e.botel@gmail.com>
2021-11-02 22:06:51 -0400

    added apidatabse route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
-const app = require("./app");
-const port = 3000;
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, '../client')));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //cors
+  next();
+});
+// put routes for database here
+app.use('/apidatabse', databaseRouter);
 
 
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 
-app.listen(port, () => console.log(`Listening on Port ${port}`));
-module.exports = port;
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
