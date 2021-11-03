3969645d22f5cde4ca5ed2cf76a95098d6624efe
shamilahfaria <42042020+shamilahfaria@users.noreply.github.com>
2021-11-03 16:55:41 -0400

    added drop functionality

--- src/Connection.jsx
+++ src/Connection.jsx
@@ -4,10 +4,17 @@ import { useDrag } from 'react-dnd';
 export default function Connection ({id, url}) {
     const [{isDragging}, drag] = useDrag(() => ({
         type: "connectionImage",
+        item: {id},
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging(),
         })
     }));
-return <img className='connectionImage' ref = {drag} src = {url}/>
-
+return (
+    <img 
+    className='connectionImage' 
+    ref = {drag} 
+    src = {url}
+    style = {{ border: isDragging ? '3px solid white' : '0px'}}
+    />
+)
 }
--- src/ConnectionsPage.jsx
+++ src/ConnectionsPage.jsx
@@ -1,58 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import Connection from "./Connection";
-
-const group = [
-  {
-    id: 4,
-    url: 'https://media-exp1.licdn.com/dms/image/C4E03AQG6IUJLyqXFjg/profile-displayphoto-shrink_800_800/0/1635467283387?e=1641427200&v=beta&t=EJn5xcVZ8qI9qFWSg4GiMkZ8IOeNaHJuArJOIrefwiQ',
-}
-];
+import {useDrop} from "react-dnd";
 
 // data from backend
 // hardcoded for now
 const ConnectionList = [
-    {
-        id: 1,
-        url: 'https://media-exp1.licdn.com/dms/image/C4E03AQGVhVOrOykpVg/profile-displayphoto-shrink_800_800/0/1620960712854?e=1641427200&v=beta&t=0DjyYOExOB02i0Hp7FKVXKD_XIetFlcM1KQBG94pvAE',
-    },
-    {
-        id: 2,
-        url: 'https://media-exp1.licdn.com/dms/image/C4E03AQE9g3iBziidTg/profile-displayphoto-shrink_800_800/0/1610317078397?e=1641427200&v=beta&t=Jn5CWuyUrYC49rEtNWid3QAvgcnc2dPu_XWmC8mlUXc',
-    },
-    {
-        id: 3,
-        url: 'https://media-exp1.licdn.com/dms/image/C4D03AQHSEiRMOsTa3A/profile-displayphoto-shrink_800_800/0/1525393074164?e=1641427200&v=beta&t=tzyrl1oErsEB2TzjGcQo1HLFUvLNrsPtYgVsKpi1jWc',
-    },
-    {
-        id: 4,
-        url: 'https://media-exp1.licdn.com/dms/image/C4E03AQG6IUJLyqXFjg/profile-displayphoto-shrink_800_800/0/1635467283387?e=1641427200&v=beta&t=EJn5xcVZ8qI9qFWSg4GiMkZ8IOeNaHJuArJOIrefwiQ',
-    },
-
+  {
+    id: 1,
+    url: "https://media-exp1.licdn.com/dms/image/C4E03AQGVhVOrOykpVg/profile-displayphoto-shrink_800_800/0/1620960712854?e=1641427200&v=beta&t=0DjyYOExOB02i0Hp7FKVXKD_XIetFlcM1KQBG94pvAE",
+  },
+  {
+    id: 2,
+    url: "https://media-exp1.licdn.com/dms/image/C4E03AQE9g3iBziidTg/profile-displayphoto-shrink_800_800/0/1610317078397?e=1641427200&v=beta&t=Jn5CWuyUrYC49rEtNWid3QAvgcnc2dPu_XWmC8mlUXc",
+  },
+  {
+    id: 3,
+    url: "https://media-exp1.licdn.com/dms/image/C4D03AQHSEiRMOsTa3A/profile-displayphoto-shrink_800_800/0/1525393074164?e=1641427200&v=beta&t=tzyrl1oErsEB2TzjGcQo1HLFUvLNrsPtYgVsKpi1jWc",
+  },
+  {
+    id: 4,
+    url: "https://media-exp1.licdn.com/dms/image/C4E03AQG6IUJLyqXFjg/profile-displayphoto-shrink_800_800/0/1635467283387?e=1641427200&v=beta&t=EJn5xcVZ8qI9qFWSg4GiMkZ8IOeNaHJuArJOIrefwiQ",
+  },
 ];
 
-
 export default function ConnectionsPage() {
+  
+
+  const [group, setGroup] = useState([
+    
+    {
+      Name: "Kinda Like",
+      Contact: "Monthly",
+      Connections: [
+        {
+          id: 1,
+          url: "https://media-exp1.licdn.com/dms/image/C4E03AQGVhVOrOykpVg/profile-displayphoto-shrink_800_800/0/1620960712854?e=1641427200&v=beta&t=0DjyYOExOB02i0Hp7FKVXKD_XIetFlcM1KQBG94pvAE",
+        },
+        {
+          id: 2,
+          url: "https://media-exp1.licdn.com/dms/image/C4E03AQE9g3iBziidTg/profile-displayphoto-shrink_800_800/0/1610317078397?e=1641427200&v=beta&t=Jn5CWuyUrYC49rEtNWid3QAvgcnc2dPu_XWmC8mlUXc",
+        },
+      ],
+    },
+  ]);
+
+  const [{isOver}, drop ] = useDrop(() => ({
+    accept: "connectionImage",
+    drop: (item) => addConnectionToBoard(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver()
+    })
+  }));
+
+  const addConnectionToBoard = (connectionId) => { 
+    const groupConnections = ConnectionList.filter((connection) => connectionId === connection.id);
+    setGroup((group) => [...group, groupConnections[0]])
+   };
+
   return (
-    <body className = 'connectionsPage'>
+    <body className="connectionsPage">
       <p> This is the Connections page. </p>
-        <div id="group">
-          <h1> Groups</h1>
-          <div className="groupsContainer" >
-          {group.map((connection) =>{
-            return <Connection url={connection.url} id={connection.id} />
-          })
-          
-          }
-          </div>
-        </div>
-        <div id="connections">
-          <h1> Connections </h1>
-          <div className="connectionsContainer">
-            {ConnectionList.map((connection) => {
-              return <Connection url={connection.url} id={connection.id} />
+      <div id="group">
+        <h1> Groups</h1>
+        <div className="groupsContainer" ref = {drop}>
+          {group.map((connection) => {
+            return <Connection url={connection.url} id={connection.id} />;
           })}
-          </div>
         </div>
+      </div>
+      <div id="connections">
+        <h1> Connections </h1>
+        <div className="connectionsContainer">
+          {ConnectionList.map((connection) => {
+            return <Connection url={connection.url} id={connection.id} />;
+          })}
+        </div>
+      </div>
     </body>
   );
 }
