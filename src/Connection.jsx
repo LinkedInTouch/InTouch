a12c2006e202fec3dfead931f0b294dafc5c7cd4
laurabotel <laura.e.botel@gmail.com>
2021-11-03 18:08:41 -0400

    updated modal to display connection name

--- src/Connection.jsx
+++ src/Connection.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useDrag } from 'react-dnd'; 
+import { prependOnceListener } from "superagent";
+import Modal from "./Modal";
+import useModal from "./useModal";
 
-export default function Connection ({id, url}) {
+export default function Connection (props) {
+    const {isVisible, toggleModal} = useModal();
+    console.log(props.firstName);
     const [{isDragging}, drag] = useDrag(() => ({
         type: "connectionImage",
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging(),
         })
     }));
-return <img className='connectionImage' ref = {drag} src = {url}/>
+    return (
+        <span>
+            <img onClick={toggleModal} className='connectionImage' ref = {drag} src = {props.url}/>
+            <Modal firstName={props.firstName} isVisible={isVisible} hideModal={toggleModal}/>
+        </span>
+    )
 
 }
--- src/ConnectionsPage.jsx
+++ src/ConnectionsPage.jsx
@@ -1,54 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Connection from "./Connection";
-// import axios from 'axios';
 
-const group = [
-  {
-    id: 4,
-    url: 'https://media-exp1.licdn.com/dms/image/C4E03AQG6IUJLyqXFjg/profile-displayphoto-shrink_800_800/0/1635467283387?e=1641427200&v=beta&t=EJn5xcVZ8qI9qFWSg4GiMkZ8IOeNaHJuArJOIrefwiQ',
-}
-];
 
-let ConnectionList = [
-    {
-        id: 1,
-        url: 'https://media-exp1.licdn.com/dms/image/C4E03AQGVhVOrOykpVg/profile-displayphoto-shrink_800_800/0/1620960712854?e=1641427200&v=beta&t=0DjyYOExOB02i0Hp7FKVXKD_XIetFlcM1KQBG94pvAE',
-    },
-    {
-        id: 2,
-        url: 'https://media-exp1.licdn.com/dms/image/C4E03AQE9g3iBziidTg/profile-displayphoto-shrink_800_800/0/1610317078397?e=1641427200&v=beta&t=Jn5CWuyUrYC49rEtNWid3QAvgcnc2dPu_XWmC8mlUXc',
-    },
-    {
-        id: 3,
-        url: 'https://media-exp1.licdn.com/dms/image/C4D03AQHSEiRMOsTa3A/profile-displayphoto-shrink_800_800/0/1525393074164?e=1641427200&v=beta&t=tzyrl1oErsEB2TzjGcQo1HLFUvLNrsPtYgVsKpi1jWc',
-    },
-    {
-        id: 4,
-        url: 'https://media-exp1.licdn.com/dms/image/C4E03AQG6IUJLyqXFjg/profile-displayphoto-shrink_800_800/0/1635467283387?e=1641427200&v=beta&t=EJn5xcVZ8qI9qFWSg4GiMkZ8IOeNaHJuArJOIrefwiQ',
-    },
+// console.log(ConnectionList);
 
-];
+// line 44
+export default function ConnectionsPage(props) {  
+  const group = [
+    {
+      id: 4,
+      url: 'https://media-exp1.licdn.com/dms/image/C4E03AQGVhVOrOykpVg/profile-displayphoto-shrink_800_800/0/1620960712854?e=1641427200&v=beta&t=0DjyYOExOB02i0Hp7FKVXKD_XIetFlcM1KQBG94pvAE',
+  }
+  ];
+  
+  let ConnectionList = [
+      {
+          id: 1,
+          url: 'https://media-exp1.licdn.com/dms/image/C4E03AQG6IUJLyqXFjg/profile-displayphoto-shrink_800_800/0/1635467283387?e=1641427200&v=beta&t=EJn5xcVZ8qI9qFWSg4GiMkZ8IOeNaHJuArJOIrefwiQ',
+      },
+  ];
+  
+  const [connectionsArr, setConnectionsArr] = useState([]);
 
-const getConnections = (userId) => {
-// takes some user id
-  fetch(`/api/database/${userId}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  const getConnections = async (userId) => {
+  // takes some user id
+    await fetch(`/api/database/${userId}`)
       .then(data => data.json())
       .then(data => {
         // console.log(data.connections);
         ConnectionList = data.connections; 
+        // console.log('after fetch', ConnectionList);
       });
-} 
-
-getConnections(1002);
-console.log(ConnectionList);
-
-
-
-
-// line 44
-export default function ConnectionsPage() {
+    // console.log('outside of fetch', ConnectionList);
+    setConnectionsArr(ConnectionList);
+  } 
   
-  
+  console.log(getConnections(1002));
   return ( //we need to generate an array of objects before the return statement where each object represents a connection
     <body className = 'connectionsPage'>
       <p> This is the Connections page. </p>
@@ -65,8 +71,8 @@ export default function ConnectionsPage() {
         <div id="connections">
           <h1> Connections </h1>
           <div className="connectionsContainer">
-            {ConnectionList.map((connection) => {
-              return <Connection url={connection.url} id={connection.id} />
+            {connectionsArr.map((connection) => {
+              return <Connection url='https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png' id={connection.id} company={connection.company} dateConnected={connection.date_connected} firstName={connection.first_name} lastName={connection.last_name} groupId={connection.group_id} notes={connection.notes} position={connection.position} quality={connection.quality} profilePic={connection.profile_picture}/>
           })}
           </div>
         </div>
